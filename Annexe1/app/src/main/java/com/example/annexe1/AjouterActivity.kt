def9f9d63cd40e7c9dee1ca8d9078b698e4f1ef2
jetcouper <ant7945@gmail.com>
2025-08-25 22:26:30 -0400

    Lundi 22h00

--- Annexe1/app/src/main/java/com/example/annexe1/AjouterActivity.kt
+++ Annexe1/app/src/main/java/com/example/annexe1/AjouterActivity.kt
@@ -1,12 +1,22 @@
 package com.example.annexe1
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
+
 class AjouterActivity : AppCompatActivity() {
+
+    lateinit var boutonAjouter: Button;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -14,12 +24,28 @@ class AjouterActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            boutonAjouter = findViewById(R.id.btnAjouterMemo);
+
+            val ec = Ecouteur();
+            boutonAjouter.setOnClickListener(ec)
+
+
             insets
         }
     }
+    // Inner class listener (like your Java version)
+    private inner class Ecouteur : View.OnClickListener {
+        override fun onClick(v: View?) {
+            if (v === boutonAjouter) {
+                val i = Intent(this@AjouterActivity, MainActivity::class.java)
+                startActivity(i)
+            }
+        }
+    }
 
-
-    
-
-
+    override fun onStop() {
+        super.onStop()
+        finish()
+    }
 }
