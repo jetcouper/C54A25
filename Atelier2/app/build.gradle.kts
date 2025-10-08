5efdab7a44c954d2f2eb3eb7595307d9a6bc5cab
jetcouper <ant7945@gmail.com>
2025-10-07 22:40:21 -0400

    Mardi 07-10-2025 Youtube

--- Atelier2/app/build.gradle.kts
+++ Atelier2/app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0") //Je l'ai ajouter moi-même, ne fait pas partie du projet
     implementation ("androidx.media3:media3-exoplayer:1.4.1")
     implementation ("androidx.media3:media3-ui:1.4.1")
     implementation ("androidx.media3:media3-common:1.4.1")
--- Atelier2/app/src/main/java/com/example/atelier2/MainActivity.kt
+++ Atelier2/app/src/main/java/com/example/atelier2/MainActivity.kt
@@ -9,10 +9,16 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var lecteur: PlayerView
+    lateinit var lecteurYoutube: YouTubePlayerView
     val mp3url = "https://storage.googleapis.com/uamp/The_Kyoto_Connection_-_Wake_Up/01_-_Intro_-_The_Way_Of_Waking_Up_feat_Alan_Watts.mp3"
+    val video = "https://youtu.be/16y1AkoZkmQ"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +30,30 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         lecteur = findViewById(R.id.playerView)
-
+        lecteurYoutube = findViewById(R.id.youtube_player_view)
+        //Lecteur audio
         val player = ExoPlayer.Builder(this@MainActivity).build()
-
         lecteur.player = player
-
         val media = MediaItem.fromUri(mp3url)
-
         player.addMediaItem(media)
-
         player.prepare()
-        player.play()
+        player.pause()
+
+        //lecteur youtube (Fait par moi)
+        lifecycle.addObserver(lecteurYoutube)
+        val ec = Ecouteur()
+        lecteurYoutube.addYouTubePlayerListener(ec)
+
 
     }
+    //Fait par moi.
+    inner class Ecouteur : AbstractYouTubePlayerListener(){
+        override fun onReady(youTubePlayer: YouTubePlayer) {
+            val videoId = "16y1AkoZkmQ"
+            youTubePlayer.loadVideo(videoId,0f)
+        }
+    }
+
+
+
 }
